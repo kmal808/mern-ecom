03e1cb8910500b9ce5e9820e75a4c7bed9f44343
kmal808 <kjmalley7@gmail.com>
2022-06-15 20:52:06 -1000

    added data files and schemas

---
+++ backend/models/userModel.js
@@ -0,0 +1,31 @@
+import mongoose from 'mongoose'
+
+const userSchmea = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    isAdmin: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+)
+
+const User = mongoose.Model('User', userSchmea)
+
+export default User
