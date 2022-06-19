c176e1913798432815d7506fc5fab95dfb6dfff4
kmal808 <kjmalley7@gmail.com>
2022-06-18 20:06:42 -1000

    added redux and started with reducers

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,10 +1,18 @@
 import { configureStore } from '@reduxjs/toolkit'
 import thunk from 'redux-thunk'
+import { productListReducer } from './reducers/productReducers'
+
+const initialState = {}
+const reducer = {
+  productList: productListReducer,
+}
+const middleware = [thunk]
 
 const store = configureStore({
-  reducer: {},
-  preloadState: {},
-  middleware: [thunk],
+  reducer,
+  middleware,
+  devTools: process.env.NODE_ENV !== 'production',
+  preloadedState: initialState,
 })
 
 export default store
