bdecd91569eba98212c8662b67861cc187564080
kmal808 <kjmalley7@gmail.com>
2022-06-20 21:56:17 -1000

    product being stored in cart when add to cart is clicked

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,6 +1,27 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { Link, useParams } from 'react-router-dom'
+import { useNavigate, useLocation } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { Row, Col, ListGroup, Image, Form, Button, Card } from 'react-bootstrap'
+import Message from '../components/Message'
+import { addToCart } from '../actions/cartActions'
 
 const CartScreen = () => {
+  const { id } = useParams()
+  const navigate = useNavigate()
+  const location = useLocation()
+  const qty = location.search ? Number(location.search.split('=')[1]) : 1
+  const dispatch = useDispatch()
+
+  const cart = useSelector((state) => state.cart)
+  const { cartItems } = cart
+
+  useEffect(() => {
+    if (id) {
+      dispatch(addToCart(id, qty))
+    }
+  }, [dispatch, id, qty])
+
   return <div>CartScreen</div>
 }
 
