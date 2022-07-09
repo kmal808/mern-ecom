53f90483cd9ebc47ba98189c7758e52dc78b3916
kmal808 <kjmalley7@gmail.com>
2022-07-08 22:25:11 -1000

    implemented get user profile route

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,9 +2,10 @@ import asyncHandler from 'express-async-handler'
 import generateToken from '../utils/generateToken.js'
 import User from '../models/userModel.js'
 
-// @desc    Auth user & get token
-// @route   POST /api/users/login
-// @access  Public
+//* @desc    Auth user & get token
+//* @route   POST /api/users/login
+//* @access  Public
+
 const authUser = asyncHandler(async (req, res) => {
 	const { email, password } = req.body
 
@@ -16,7 +17,7 @@ const authUser = asyncHandler(async (req, res) => {
 			name: user.name,
 			email: user.email,
 			isAdmin: user.isAdmin,
-			token: generateToken(user._id),
+			token: generateToken,
 		})
 	} else {
 		res.status(401)
@@ -24,4 +25,26 @@ const authUser = asyncHandler(async (req, res) => {
 	}
 })
 
-export { authUser }
+//* @desc    Get user profile
+//* @route   GET /api/users/login
+//* @access  Private
+
+const getUserProfile = asyncHandler(async (req, res) => {
+	res.send('Success')
+	// const user = await User.findById()
+
+	// if (user && (await user.matchPassword(password))) {
+	// 	res.json({
+	// 		_id: user._id,
+	// 		name: user.name,
+	// 		email: user.email,
+	// 		isAdmin: user.isAdmin,
+	// 		token: generateToken,
+	// 	})
+	// } else {
+	// 	res.status(401)
+	// 	throw new Error('Invalid email or password')
+	// }
+})
+
+export { authUser, getUserProfile }
