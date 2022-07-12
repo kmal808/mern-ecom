a000250c3fd70e6b0ff0e9d461b2f0a8cec8be96
kmal808 <kjmalley7@gmail.com>
2022-07-11 22:12:10 -1000

    user actions, constants and reducers

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,30 +1,32 @@
 import { configureStore } from '@reduxjs/toolkit'
 import thunk from 'redux-thunk'
 import {
-  productListReducer,
-  productDetailsReducer,
+	productListReducer,
+	productDetailsReducer,
 } from './reducers/productReducers'
 import { cartReducer } from './reducers/cartReducers'
+import { userLoginReducer } from './reducers/userReducers'
 
 const cartItemsFromStorage = localStorage.getItem('cartItems')
-  ? JSON.parse(localStorage.getItem('cartItems'))
-  : []
+	? JSON.parse(localStorage.getItem('cartItems'))
+	: []
 
 const initialState = {
-  cart: { cartItems: cartItemsFromStorage },
+	cart: { cartItems: cartItemsFromStorage },
 }
 const reducer = {
-  productList: productListReducer,
-  productDetails: productDetailsReducer,
-  cart: cartReducer,
+	productList: productListReducer,
+	productDetails: productDetailsReducer,
+	cart: cartReducer,
+	userLogin: userLoginReducer,
 }
 const middleware = [thunk]
 
 const store = configureStore({
-  reducer,
-  middleware,
-  devTools: process.env.NODE_ENV !== 'production',
-  preloadedState: initialState,
+	reducer,
+	middleware,
+	devTools: process.env.NODE_ENV !== 'production',
+	preloadedState: initialState,
 })
 
 export default store
