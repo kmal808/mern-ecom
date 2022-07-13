f9ede5fb0743498a5982c18c87fcae54b0111b10
kmal808 <kjmalley7@gmail.com>
2022-07-12 21:17:49 -1000

    finished userAction and added userInfo to store

---
+++ frontend/src/actions/userAction.js
@@ -0,0 +1,42 @@
+import axios from 'axios'
+import { json } from 'express'
+import {
+	USER_LOGIN_FAIL,
+	USER_LOGIN_REQUEST,
+	USER_LOGIN_SUCCESS,
+} from '../constants/userConstants'
+
+export const login = (email, password) => async (dispatch) => {
+	try {
+		dispatch({
+			type: USER_LOGIN_REQUEST,
+		})
+
+		const config = {
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		}
+
+		const { data } = await axios.post(
+			'/api/users/login',
+			{ email, password },
+			config
+		)
+
+		dispatch({
+			type: USER_LOGIN_SUCCESS,
+			payload: data,
+		})
+
+		localStorage.setItem('userInfo', JSON.stringify(data))
+	} catch (error) {
+		dispatch({
+			type: USER_LOGIN_FAIL,
+			payload:
+				error.response && error.message.data.message
+					? error.message.data.message
+					: error.message,
+		})
+	}
+}
