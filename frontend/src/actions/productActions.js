cf7a67f24dc6c8c850379ab19420b3200f89128b
kmal808 <kjmalley7@gmail.com>
2022-07-17 23:38:45 -1000

    fixing checkout redirect

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -1,47 +1,47 @@
 import axios from 'axios'
 import {
-  PRODUCT_LIST_REQUEST,
-  PRODUCT_LIST_SUCCESS,
-  PRODUCT_LIST_FAIL,
-  PRODUCT_DETAILS_REQUEST,
-  PRODUCT_DETAILS_SUCCESS,
-  PRODUCT_DETAILS_FAIL,
+	PRODUCT_LIST_REQUEST,
+	PRODUCT_LIST_SUCCESS,
+	PRODUCT_LIST_FAIL,
+	PRODUCT_DETAILS_REQUEST,
+	PRODUCT_DETAILS_SUCCESS,
+	PRODUCT_DETAILS_FAIL,
 } from '../constants/productConstants'
 
 export const listProducts = (dispatch) => {
-  const list = async (dispatch) => {
-    try {
-      dispatch({ type: PRODUCT_LIST_REQUEST })
-      const { data } = await axios.get('/api/products')
-      dispatch({ type: PRODUCT_LIST_SUCCESS, payload: data })
-    } catch (error) {
-      dispatch({
-        type: PRODUCT_LIST_FAIL,
-        payload:
-          error.response && error.response.data.message
-            ? error.response.data.message
-            : error.message,
-      })
-    }
-  }
-  list(dispatch)
+	const list = async (dispatch) => {
+		try {
+			dispatch({ type: PRODUCT_LIST_REQUEST })
+			const { data } = await axios.get('/api/products')
+			dispatch({ type: PRODUCT_LIST_SUCCESS, payload: data })
+		} catch (error) {
+			dispatch({
+				type: PRODUCT_LIST_FAIL,
+				payload:
+					error.response && error.response.data.message
+						? error.response.data.message
+						: error.message,
+			})
+		}
+	}
+	list(dispatch)
 }
 
 export const listProductDetails = (dispatch, id) => {
-  const list = async (dispatch, id) => {
-    try {
-      dispatch({ type: PRODUCT_DETAILS_REQUEST })
-      const { data } = await axios.get(`/api/products/${id}`)
-      dispatch({ type: PRODUCT_DETAILS_SUCCESS, payload: data })
-    } catch (error) {
-      dispatch({
-        type: PRODUCT_DETAILS_FAIL,
-        payload:
-          error.response && error.message.data.message
-            ? error.message.data.message
-            : error.message,
-      })
-    }
-  }
-  list(dispatch, id)
+	const list = async (dispatch, id) => {
+		try {
+			dispatch({ type: PRODUCT_DETAILS_REQUEST })
+			const { data } = await axios.get(`/api/products/${id}`)
+			dispatch({ type: PRODUCT_DETAILS_SUCCESS, payload: data })
+		} catch (error) {
+			dispatch({
+				type: PRODUCT_DETAILS_FAIL,
+				payload:
+					error.response && error.message.data.message
+						? error.message.data.message
+						: error.message,
+			})
+		}
+	}
+	list(dispatch, id)
 }
--- frontend/src/screens/homeScreen.js
+++ frontend/src/screens/homeScreen.js
@@ -7,31 +7,31 @@ import Loader from '../components/Loader'
 import { listProducts } from '../actions/productActions'
 
 const HomeScreen = () => {
-  const { products, loading, error } = useSelector((state) => state.productList)
-  const dispatch = useDispatch()
+	const { products, loading, error } = useSelector((state) => state.productList)
+	const dispatch = useDispatch()
 
-  useEffect(() => {
-    dispatch(listProducts)
-  }, [dispatch])
+	useEffect(() => {
+		dispatch(listProducts)
+	}, [dispatch])
 
-  return (
-    <>
-      <h1>Latest Products</h1>
-      {loading ? (
-        <Loader />
-      ) : error ? (
-        <Message variant='danger'>{error}</Message>
-      ) : (
-        <Row>
-          {products.map((product) => (
-            <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-              <Product product={product} />
-            </Col>
-          ))}
-        </Row>
-      )}
-    </>
-  )
+	return (
+		<>
+			<h1>Latest Products</h1>
+			{loading ? (
+				<Loader />
+			) : error ? (
+				<Message variant='danger'>{error}</Message>
+			) : (
+				<Row>
+					{products.map((product) => (
+						<Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+							<Product product={product} />
+						</Col>
+					))}
+				</Row>
+			)}
+		</>
+	)
 }
 
 export default HomeScreen
