2e3e16131147a4bc3e5db62116754763403cc1a8
kmal808 <kjmalley7@gmail.com>
2022-07-26 22:32:22 -1000

    hunting for issue/bug/type-o/something

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,5 +1,5 @@
 import asyncHandler from 'express-async-handler'
-import Order from '../models/productModel.js'
+import Order from '../models/orderModel.js'
 
 // @desc    Create new order
 // @route   POST /api/orders
@@ -17,12 +17,11 @@ const addOrderItems = asyncHandler(async (req, res) => {
 
 	if (orderItems && orderItems.length === 0) {
 		res.status(400)
-		throw new Error('No order items')
-		return
+		throw new Error('No order Items')
 	} else {
 		const order = new Order({
-			orderItems,
 			user: req.user._id,
+			orderItems,
 			shippingAddress,
 			paymentMethod,
 			itemsPrice,
