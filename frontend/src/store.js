38f15f5ada190345c6b4f0b5211ec7735a4f55e3
kmal808 <kjmalley7@gmail.com>
2022-08-17 22:09:22 -1000

    order details reducer added to redux store

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -11,7 +11,10 @@ import {
 	userRegisterReducer,
 	userUpdateProfileReducer,
 } from './reducers/userReducers'
-import { orderCreateReducer } from './reducers/orderReducers'
+import {
+	orderCreateReducer,
+	orderDetailsReducer,
+} from './reducers/orderReducers'
 
 const cartItemsFromStorage = localStorage.getItem('cartItems')
 	? JSON.parse(localStorage.getItem('cartItems'))
@@ -46,6 +49,7 @@ const reducer = {
 	userDetails: userDetailsReducer,
 	userUpdateProfile: userUpdateProfileReducer,
 	orderCreate: orderCreateReducer,
+	orderDetails: orderDetailsReducer,
 }
 const middleware = [thunk]
 
