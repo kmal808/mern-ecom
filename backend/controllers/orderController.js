01f5683d6086cacef7b0cf857779cb86a21c2eb5
kmal808 <kjmalley7@gmail.com>
2022-08-17 21:25:44 -1000

    added get order by id route

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,9 +1,9 @@
 import asyncHandler from 'express-async-handler'
 import Order from '../models/orderModel.js'
 
-// @desc    Create new order
-// @route   POST /api/orders
-// @access  Private
+//* @desc    Create new order
+//* @route   POST /api/orders
+//* @access  Private
 const addOrderItems = asyncHandler(async (req, res) => {
 	const {
 		orderItems,
@@ -20,8 +20,8 @@ const addOrderItems = asyncHandler(async (req, res) => {
 		throw new Error('No order Items')
 	} else {
 		const order = new Order({
-			user: req.user._id,
 			orderItems,
+			user: req.user._id,
 			shippingAddress,
 			paymentMethod,
 			itemsPrice,
@@ -36,4 +36,21 @@ const addOrderItems = asyncHandler(async (req, res) => {
 	}
 })
 
-export { addOrderItems }
+//* @desc    Get order by ID
+//* @route   GET /api/orders/:id
+//* @access  Private
+const getOrderById = asyncHandler(async (req, res) => {
+	const order = await Order.findById(req.params.id).populate(
+		'user',
+		'name email'
+	)
+
+	if (order) {
+		res.json(order)
+	} else {
+		res.status(404)
+		throw new Error('Order not found')
+	}
+})
+
+export { addOrderItems, getOrderById }
