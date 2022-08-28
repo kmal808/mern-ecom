93092410134229b5b9282cb47c8cafa2524c4236
kmal808 <kjmalley7@gmail.com>
2022-08-27 21:05:08 -1000

    fixed type-o in getMyOrders controller

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -82,7 +82,7 @@ const updateOrderToPaid = asyncHandler(async (req, res) => {
 //* @route   PUT /api/orders/myorders
 //* @access  Private
 const getMyOrders = asyncHandler(async (req, res) => {
-	const orders = await Order.find({ users: req.user._id })
+	const orders = await Order.find({ user: req.user._id })
 	res.json(orders)
 })
 
