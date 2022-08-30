d1e7ba060b089d39580f7235298a0aa09fcfd0b6
kmal808 <kjmalley7@gmail.com>
2022-08-29 22:23:42 -1000

    fixed casing on userlist path

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -33,7 +33,7 @@ const App = () => {
 						<Route path='/product/:id' element={<ProductScreen />} />
 						<Route path='/cart/:id' element={<CartScreen />} />
 						<Route path='/cart' element={<CartScreen />} />
-						<Route path='/admin/userList' element={<UserListScreen />} />
+						<Route path='/admin/userlist' element={<UserListScreen />} />
 					</Routes>
 				</Container>
 			</main>
