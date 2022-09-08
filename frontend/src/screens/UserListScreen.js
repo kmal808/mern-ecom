3e14e9d2adf40c9a3a4ebc0e36b3887dfe78cee7
kmal808 <kjmalley7@gmail.com>
2022-09-07 20:25:02 -1000

    added userDelete to UserListScreen

--- frontend/src/screens/UserListScreen.js
+++ frontend/src/screens/UserListScreen.js
@@ -5,7 +5,7 @@ import { Table, Button } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import Message from '../components/Message'
 import Loader from '../components/Loader'
-import { listUsers } from '../actions/userAction'
+import { listUsers, deleteUser } from '../actions/userAction'
 
 const UserListScreen = () => {
 	const dispatch = useDispatch()
@@ -17,16 +17,19 @@ const UserListScreen = () => {
 	const userLogin = useSelector((state) => state.userLogin)
 	const { userInfo } = userLogin
 
+	const userDelete = useSelector((state) => state.userDelete)
+	const { success: successDelete } = userDelete
+
 	useEffect(() => {
 		if (userInfo && userInfo.isAdmin) {
 			dispatch(listUsers())
 		} else {
 			navigate('/login')
 		}
-	}, [dispatch, navigate, userInfo])
+	}, [dispatch, navigate, userInfo, successDelete])
 
 	const deleteHandler = (id) => {
-		console.log('delete')
+		dispatch(deleteUser(id))
 	}
 
 	return (
