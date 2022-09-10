af95a5b1c9dd5d72c444b94eb9b2063195af82e3
kmal808 <kjmalley7@gmail.com>
2022-09-09 20:14:27 -1000

    added updateUser and getUserById to controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -131,6 +131,20 @@ const deleteUser = asyncHandler(async (req, res) => {
 	}
 })
 
+//* @desc    Get user by id
+//* @route   GET /api/users/:id
+//* @access  Private/Admin
+
+const getUserById = asyncHandler(async (req, res) => {
+	const user = await User.findById(req.params.id).select('-password')
+	if (user) {
+		res.json(user)
+	} else {
+		res.status(404)
+		throw new Error('User not found')
+	}
+})
+
 export {
 	authUser,
 	registerUser,
