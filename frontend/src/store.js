a03285df2721074a482861f264e14a8a6ef465ce
kmal808 <kjmalley7@gmail.com>
2022-09-21 20:33:28 -1000

    feat: added admin delete product

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -3,6 +3,7 @@ import thunk from 'redux-thunk'
 import {
 	productListReducer,
 	productDetailsReducer,
+	productDeleteReducer,
 } from './reducers/productReducers'
 import { cartReducer } from './reducers/cartReducers'
 import {
@@ -48,6 +49,7 @@ const initialState = {
 const reducer = {
 	productList: productListReducer,
 	productDetails: productDetailsReducer,
+	productDelete: productDeleteReducer,
 	cart: cartReducer,
 	userLogin: userLoginReducer,
 	userRegister: userRegisterReducer,
